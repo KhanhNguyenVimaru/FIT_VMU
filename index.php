1643ef04cec3efc8962024357da02694ddf12d56
Khánh Nguyễn <khanhnd05@gmail.com>
2025-07-26 15:50:20 +0700

    architect

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    include 'connect_db.php' // kết nối database ở đấy, địa chỉ nằm ở ngoài folder thêm ..
+?> 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Khoa Công nghệ thông tin - Trường Đại học Hàng hải Việt Nam</title>
+    <link rel="icon" type="image/x-icon" href="component/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+    <?php include 'view/component/header.php'; ?>
+    <!-- giao diện cho container -->
+    <?php include 'view/component/footer.php'; ?>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+<script src="main.js"></script>
+</html>
